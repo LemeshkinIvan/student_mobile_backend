ea8355f1e94a0bfeeb4868b296ec8093b4754ce1
Ivan Lemeshkin <106601642+LemeshkinIvan@users.noreply.github.com>
2025-06-29 23:52:11 +0300

    Delete internal/data/repository directory

--- internal/data/repository/user/user_assertion.go
+++
@@ -1,8 +0,0 @@
-package user_repository
-
-import (
-	user_repository_interface "app/internal/domain/repositories/user"
-)
-
-// явное определение контракта интерфейсов
-var _ user_repository_interface.UserRepository = (*UserRepositoryImpl)(nil)
